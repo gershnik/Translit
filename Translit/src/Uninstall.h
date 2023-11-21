daa4dfbbe1ccf83961d9474595e3c01095b72e2f
Eugene Gershnik <gershnik@hotmail.com>
2023-11-21 03:47:03 -0800

    Moving uninstallation into its own file

---
+++ Translit/src/Uninstall.h
@@ -0,0 +1,12 @@
+// Copyright (c) 2023, Eugene Gershnik
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+#ifndef TRANSLIT_HEADER_UNINSTALL_H_INCLUDED
+#define TRANSLIT_HEADER_UNINSTALL_H_INCLUDED
+
+
+auto uninstallApplication() -> int;
+
+#endif
+
+
