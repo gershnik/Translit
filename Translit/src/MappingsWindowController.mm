8ede1235a09b6897bd6a73ce0ef58c58f056d0bd
Eugene Gershnik <gershnik@users.noreply.github.com>
2025-05-21 03:52:12 -0700

    HTML rendering of mappings table

--- Translit/src/MappingsWindowController.mm
+++ Translit/src/MappingsWindowController.mm
@@ -4,11 +4,12 @@
 #import "MappingsWindowController.hpp"
 #import "AppDelegate.hpp"
 
-@interface MappingsWindowController () {
-    IBOutlet NSImageView * _text;
+@interface MappingsWindowController () <WKNavigationDelegate> {
+    IBOutlet WKWebView * _text;
     IBOutlet NSPopUpButton * _schemeSelector;
     NSString * _language;
     NSString * _variant;
+    bool _firstLoad;
 }
 
 @end
@@ -18,8 +19,8 @@
 
 -(void) windowDidLoad {
     [super windowDidLoad];
-    [_text setWantsLayer:true];
-    _text.layer.backgroundColor = NSColor.whiteColor.CGColor;
+    _text.navigationDelegate = self;
+    _firstLoad = true;
     [self showLanguage:_language];
 }
 
@@ -52,6 +53,7 @@
     [_schemeSelector selectItem:selected];
     [_schemeSelector synchronizeTitleAndSelectedItem];
     
+    _firstLoad = true;
     [self updateTable];
 }
 
@@ -71,9 +73,44 @@
     auto name = [@"mapping-" stringByAppendingString:_language];
     if (_variant.length)
         name = [[name stringByAppendingString:@"."] stringByAppendingString:_variant];
-    auto url = [NSBundle.mainBundle URLForResource:name withExtension:@"pdf"];
-    auto * image = [[NSImage alloc] initWithContentsOfURL:url];
-    _text.image = image;
+    auto url = [NSBundle.mainBundle URLForResource:name withExtension:@"html"];
+    auto html = [NSString stringWithContentsOfURL:url encoding:NSUTF8StringEncoding error:nil];
+    [_text loadHTMLString:html baseURL:nil];
+}
+
+-(void) webView:(WKWebView *)webView didFinishNavigation:(WKNavigation *)navigation {
+    [webView evaluateJavaScript:@"document.getElementById('table-holder').scrollHeight"
+              completionHandler:^(id result, NSError * err){
+        dispatch_async(dispatch_get_main_queue(), ^{
+            if (err)
+                return;
+            auto height = (NSNumber *)result;
+            auto contentRect = self.window.contentLayoutRect;
+            auto textFrame = self->_text.frame;
+            auto minSize = self.window.contentMinSize;
+            minSize.height = (contentRect.size.height - textFrame.size.height) + height.doubleValue;
+            os_log_info(OS_LOG_DEFAULT, "HAHAHA %{public}lf %{public}lf", contentRect.size.height, minSize.height);
+            [self.window setContentMinSize:minSize];
+            if (self->_firstLoad || contentRect.size.height < minSize.height)
+                [self.window setContentSize:{contentRect.size.width, minSize.height}];
+            self->_firstLoad = false;
+        });
+    }];
+}
+
+-(void) webView:(WKWebView *)webView
+        decidePolicyForNavigationAction:(WKNavigationAction *)navigationAction
+                        decisionHandler:(void (^)(WKNavigationActionPolicy))decisionHandler {
+    
+    if (navigationAction.navigationType == WKNavigationTypeLinkActivated) {
+        auto url = navigationAction.request.URL;
+        os_log_debug(OS_LOG_DEFAULT, "Navigating to link:  %{public}@", url);
+        [NSWorkspace.sharedWorkspace openURL:url];
+        decisionHandler(WKNavigationActionPolicyCancel);
+    } else {
+        decisionHandler(WKNavigationActionPolicyAllow);
+    }
+    
 }
 
 @end
